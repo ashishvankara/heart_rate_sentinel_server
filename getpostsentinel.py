23b10dc87dee9ee39a83c8a5fccbaa672735fdb8
Ashish Vankara <ashishvankara@gmail.com>
2018-11-16 11:36:19 -0500

    added interval average + testing

--- getpostsentinel.py
+++ getpostsentinel.py
@@ -1,8 +1,7 @@
 import requests
-#import json
+import datetime
 
-a = requests.get("http://127.0.0.1:5000/")
-print(a.text)
+hello = requests.get("http://127.0.0.1:5000/")
 
 r1 = requests.post("http://127.0.0.1:5000/new_patient",
                    json={"patient_id": "123",
@@ -10,13 +9,14 @@ r1 = requests.post("http://127.0.0.1:5000/new_patient",
                          "user_age": 2}
                    )
 
-r2 = requests.get("http://127.0.0.1:5000/data")
-print(r2.json())
+#r2 = requests.get("http://127.0.0.1:5000/data")
+#print(r2.json())
 
-r3= requests.post("http://127.0.0.1:5000/heart_rate",
+r3 = requests.post("http://127.0.0.1:5000/heart_rate",
                    json={"patient_id": "123",
                          "heart_rate": 300})
 
+r4 = requests.get("http://127.0.0.1:5000/status/123")
 r4 = requests.get("http://127.0.0.1:5000/data")
 print(r4.json())
 
@@ -26,12 +26,25 @@ print(r5.text)
 r6 = requests.get("http://127.0.0.1:5000/heart_rate/123")
 print(r6.text)
 
-r7= requests.post("http://127.0.0.1:5000/heart_rate",
+a = datetime.datetime.now()
+
+r7 = requests.post("http://127.0.0.1:5000/heart_rate",
                    json={"patient_id": "123",
                          "heart_rate": 0})
+b = datetime.datetime.now()
 
-r8 = r6 = requests.get("http://127.0.0.1:5000/heart_rate/average/123")
-print(r6.text)
+r8 = requests.post("http://127.0.0.1:5000/heart_rate",
+                   json={"patient_id": "123",
+                         "heart_rate": 300})
+
+r = requests.get("http://127.0.0.1:5000/heart_rate/average/123")
+
+# Can change between a and b to test interval avg
+
+rsince = requests.post("http://127.0.0.1:5000/heart_rate/interval_average",
+                   json={"patient_id": "123",
+                         "heart_rate_average_since": a.isoformat()})
+print(rsince.text)
 
 
 
--- hrserver.py
+++ hrserver.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify, request
 from pymodm import MongoModel, fields, connect
-import datetime
+from datetime import datetime
+import sendgrid
+import numpy as np
+from sendgrid.helpers.mail import *
 
 connect("mongodb://av135:Dukebm3^@ds039778.mlab.com:39778/hr_sentinel")
 
@@ -59,34 +62,53 @@ def greeting():
 
 
 
-@app.route("/data", methods=["GET"])
-def getData():
+@app.route("/data/<patient_id>", methods=["GET"])
+def getData(patient_id):
+    """ Returns the data dictionary below to the caller as JSON
+
+    This function returns all the stored information for a patient as a JSON dictionary
+
+    Args:
+        patient_id (string): string specifying patient MRN.
+
+    Returns:
+        dict_array (dict): Dictionary of stored information for specified patient
     """
-    Returns the data dictionary below to the caller as JSON
-    """
-    first_user = User.objects.raw({"_id": "123"}).first()
-    dict_array = {"patient_id": first_user.mrn,
-                  "attending_email": first_user.attending_email,
-                  "user_age": first_user.user_age,
-                  "heart_rate": first_user.heart_rate,
-                  "heart_rate_timestamp": first_user.heart_rate_timestamp
+
+    u = User.objects.raw({"_id": patient_id}).first()
+    dict_array = {"patient_id": u.mrn,
+                  "attending_email": u.attending_email,
+                  "user_age": u.user_age,
+                  "heart_rate": u.heart_rate,
+                  "heart_rate_timestamp": u.heart_rate_timestamp
                   }
     return jsonify(dict_array)
 
 @app.route("/status/<patient_id>", methods=["GET"])
 def getStatus(patient_id):
-    """
-    Returns the data dictionary below to the caller as JSON
+    """ Returns whether patient is tachycardic based on previously available data
+
+    This function returns whether the specified patient is currently tachycardic based on previously available
+     heart rate data, as well as the timestamp of the most recent heart rate.
+
+    Args:
+        patient_id (string): string specifying patient MRN.
+
+    Returns:
+        ans (string): Specifying if patient is tachycardic
     """
     u = User.objects.raw({"_id": patient_id}).first()
     most_recent_hr = u.heart_rate[-1]
     most_recent_time = u.heart_rate_timestamp[-1]
     tachy = istachycardic(u.user_age, most_recent_hr)
-    ans = "Patient is {} His/Her heart rate is {} based on data from {}"
+    ansraw = "Patient is {} His/Her heart rate is {} based on data from {}"
     if tachy:
-        return ans.format("tachycardic!", most_recent_hr, most_recent_time)
+        ans = ansraw.format("tachycardic!", most_recent_hr, most_recent_time)
+        a = email('av135_hr_sentinel@duke.edu', u.attending_email, 'Urgent patient #{}!'.format(u.mrn), ans)
+        return ans
     else:
-        return ans.format("not tachycardic.", most_recent_hr, most_recent_time)
+        ans = ansraw.format("not tachycardic.", most_recent_hr, most_recent_time)
+        return ans
 
 @app.route("/heart_rate/<patient_id>", methods=["GET"])
 def getHR(patient_id):
@@ -103,9 +125,9 @@ def getavgHR(patient_id):
     Returns the data dictionary below to the caller as JSON
     """
     u = User.objects.raw({"_id": patient_id}).first()
-    total_stored_hr_data = u.heart_rate[1:]
+    total_stored_hr_data = u.heart_rate[0:]
     avghr = averageHR(total_stored_hr_data)
-    ans =" The patient's average heart over all stored measurements is {}."
+    ans ="The patient's average heart over all stored measurements is {}."
     return ans.format(avghr)
 
 @app.route("/heart_rate", methods=["POST"])
@@ -113,14 +135,17 @@ def addHr():
     data_in = request.get_json()
     u = User.objects.raw({"_id": data_in["patient_id"]}).first()
     u.heart_rate.append(data_in["heart_rate"])
-    currenttime = datetime.datetime.now().time()
+    currenttime = datetime.now()
     u.heart_rate_timestamp.append(currenttime.isoformat())
+    if u.heart_rate[0] == 0:   # Removes initialization value
+        u.heart_rate.pop(0)
+        u.heart_rate_timestamp.pop(0)
     u.save()
     return u.mrn
 
 @app.route("/new_patient", methods=["POST"])
 def addpatient():
-    now = datetime.datetime.now().time()
+    now = datetime.now()
     data_in = request.get_json()
     u = User(mrn=data_in["patient_id"],
              attending_email=data_in["attending_email"],
@@ -130,6 +155,36 @@ def addpatient():
              )
     u.save()
     return u.mrn
+
+@app.route("/heart_rate/interval_average", methods=["POST"])
+def intervalaverage():
+    data_in = request.get_json()
+    u = User.objects.raw({"_id": data_in["patient_id"]}).first()
+    hr_since = datetime.strptime(data_in["heart_rate_average_since"], "%Y-%m-%dT%H:%M:%S.%f")# includes data at specified time point
+    hr_time_stamp_datetime = [datetime.strptime(i, "%Y-%m-%dT%H:%M:%S.%f") for i in u.heart_rate_timestamp]
+    hr_time_stamp_array = np.asarray(hr_time_stamp_datetime)
+    hr_array = np.asarray(u.heart_rate)
+    adjusted_hr_data = recondition(hr_since, hr_time_stamp_array, hr_array)
+    avg = averageHR(adjusted_hr_data)
+    ans ="The patient's average heart since {} is {}."
+    return ans.format(data_in["heart_rate_average_since"], avg)
+
+def email(from_email_string, to_email_string, subject, content_string):
+    key = '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'
+    sg = sendgrid.SendGridAPIClient(apikey=key)
+    from_email = Email(from_email_string)
+    to_email = Email(to_email_string)
+    content = Content("text/plain", content_string)
+    mail = Mail(from_email, subject, to_email, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    return response.status_code
+
+def recondition(heart_rate_average_since, heart_rate_timestamp, heart_rate):
+    relevant_indices = (heart_rate_timestamp >= heart_rate_average_since).nonzero()
+    reconditioned_hr_data = heart_rate[relevant_indices].tolist()
+    return reconditioned_hr_data
+
+
 
 
 
