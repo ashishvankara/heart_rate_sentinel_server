1ef143bdb292422c57f50bcaa2159510fe41c053
Ashish Vankara <ashishvankara@gmail.com>
2018-11-16 19:06:58 -0500

    adhered to pep8 conventions

--- test_hrserver.py
+++ test_hrserver.py
@@ -1,6 +1,7 @@
 import pytest
 
-@pytest.mark.parametrize("user_age, hr, expected",[
+
+@pytest.mark.parametrize("user_age, hr, expected", [
     (10/365, 200, True),
     (10/365, 170, False),
     (1.5, 170, True),
@@ -16,6 +17,7 @@ def test_istachycardic(user_age, hr, expected):
     a = istachycardic(user_age, hr)
     assert a == expected
 
+
 def test_averageHR():
     """ Tests averageHR() calculations
 
@@ -26,6 +28,7 @@ def test_averageHR():
     calculated_avg = averageHR(a)
     assert pytest.approx(calculated_avg) == a.mean()
 
+
 def test_recondition():
     """ Tests recondition()
 
@@ -33,9 +36,10 @@ def test_recondition():
     import numpy as np
     from datetime import datetime, timedelta
     from hrserver import recondition
-    t = np.arange(datetime(2018, 11, 11, 12, 1), datetime(2018, 11, 11, 12, 6), timedelta(minutes=1)).astype(datetime)
+    t = np.arange(datetime(2018, 11, 11, 12, 1),
+                  datetime(2018, 11, 11, 12, 6),
+                  timedelta(minutes=1)).astype(datetime)
     hr_since = datetime(2018, 11, 11, 12, 3)
     mockhr = np.arange(0, 5)
     reconditioned_hr = recondition(hr_since, t, mockhr)
     assert reconditioned_hr == [2, 3, 4]
-
